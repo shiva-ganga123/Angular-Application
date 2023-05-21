109236c304d56f24653027ad12c52c23c64ede21
shiva gupta <shivagupta8019@gmail.com>
2023-05-21 13:55:53 -0400

    modified cart-page

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,18 +1,22 @@
 import { CartItem } from './../shared/models/cartItem';
 import { CartService } from './../services/cart.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cart } from '../shared/models/Cart';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
 cart!: Cart;
 
-constructor(private cartService: CartService){
-  this.setCart();
+constructor(private cartService: CartService, private foodService: FoodService){
+    let foods = foodService.getAll();
+    cartService.addToCart(foods[1]);
+    cartService.addToCart(foods[3]);
+    this.setCart();
 }
 
 ngOnInit(): void{
