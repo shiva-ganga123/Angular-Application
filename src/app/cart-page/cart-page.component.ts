6213762f761e3efec27edb2b0871eb0b5baf18cd
shiva gupta <shivagupta8019@gmail.com>
2023-05-23 09:49:24 -0400

    Added Checkout button on cart-page

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -12,10 +12,10 @@ import { FoodService } from '../services/food/food.service';
 export class CartPageComponent implements OnInit {
 cart!: Cart;
 
-constructor(private cartService: CartService){
-    // let foods = foodService.getAll();
-    // cartService.addToCart(foods[1]);
-    // cartService.addToCart(foods[3]);
+constructor(private cartService: CartService, private foodService: FoodService){
+    let foods = foodService.getAll();
+    cartService.addToCart(foods[1]);
+    cartService.addToCart(foods[3]);
     this.setCart();
 }
 
