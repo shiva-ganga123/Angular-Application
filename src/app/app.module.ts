06c304a5d5b5fbd5d8828ae0eef09a45b1ace004
shiva gupta <shivagupta8019@gmail.com>
2023-05-23 10:52:48 -0400

    Added nor found componennt to the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SearchComponent } from './search/search.component';
 import { TagsComponent } from './tags/tags.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { FoodpageComponent } from './foodpage/foodpage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -28,6 +29,7 @@ import { FoodpageComponent } from './foodpage/foodpage.component';
     TagsComponent,
     CartPageComponent,
     FoodpageComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
